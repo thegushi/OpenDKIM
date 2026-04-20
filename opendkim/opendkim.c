5fe18fd49a909cefece512c27425d6a876d1f8a4
Dan Mahoney <github@gushiorg>
2026-04-20 13:48:01 -0700

    Fix misleading error message in dkimf_checkfsnode directory check
    When a directory in the key path failed the group safety check, the
    error message used the running username instead of the directory path,
    making it impossible to tell which directory was the problem.

--- opendkim/opendkim.c
+++ opendkim/opendkim.c
@@ -4608,7 +4608,7 @@ dkimf_checkfsnode(const char *path, uid_t myuid, char *myname, ino_t *ino,
 					{
 						snprintf(err, errlen,
 						         "%s is in group %u which has multiple users (e.g., \"%s\")",
-						         myname, s.st_gid,
+						         path, s.st_gid,
 						         pw->pw_name);
 					}
 
@@ -4634,7 +4634,7 @@ dkimf_checkfsnode(const char *path, uid_t myuid, char *myname, ino_t *ino,
 					{
 						snprintf(err, errlen,
 						         "%s is in group %u which has multiple users (e.g., \"%s\")",
-						         myname, s.st_gid,
+						         path, s.st_gid,
 						         gr->gr_mem[c]);
 					}
 
